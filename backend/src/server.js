516c0e2489b518450c2128e983b06af1548a3187
piriyajeishree410 <priyajeishree410@gmail.com>
2025-11-11 10:01:51 -0500

    Making cookie secure

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,6 +14,7 @@ import leaderboardRouter from "./routes/leaderboard.routes.js";
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1); 
 app.use(express.json());
 
 const ALLOWED_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:5173";
