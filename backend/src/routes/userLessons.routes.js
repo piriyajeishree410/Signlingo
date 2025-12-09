9248fe4155a1f11365468cb280138dfdbd7ea4df
Nandana Pradeep <136226679+Nandana-125@users.noreply.github.com>
2025-12-10 05:00:14 +0530

    Update userLessons.routes.js

--- backend/src/routes/userLessons.routes.js
+++ backend/src/routes/userLessons.routes.js
@@ -7,23 +7,24 @@ import {
   getAllUserLessons,
   getProgressForLesson,
 } from "../controllers/userLessons.controller.js";
+import { requireAuth } from "../middleware/requireAuth.js";
 
 const router = express.Router();
 
-router.get("/progress", getProgressForLesson);
+router.get("/progress",requireAuth, getProgressForLesson);
 
-router.get("/", getAllUserLessons);
+router.get("/",requireAuth, getAllUserLessons);
 
 // CREATE - start a new lesson
-router.post("/start", startLesson);
+router.post("/start",requireAuth, startLesson);
 
 // READ - get all lessons for user
-router.get("/", getUserLessons);
+router.get("/",requireAuth, getUserLessons);
 
 // UPDATE - mark sign as done / complete lesson / add XP
-router.put("/:lessonId/progress", updateProgress);
+router.put("/:lessonId/progress",requireAuth, updateProgress);
 
 // DELETE - reset progress
-router.delete("/:lessonId/reset", resetLesson);
+router.delete("/:lessonId/reset",requireAuth, resetLesson);
 
 export default router;
